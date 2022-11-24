075657a8b57ceabb2c143ea7f9c2cc94e5be7008
Andreea Rascanu <arascanu@stpaulscc.co.uk>
2022-11-24 08:32:38 +0000

    serve from CDN

--- src/extensions/customBranding/CustomBrandingApplicationCustomizer.ts
+++ src/extensions/customBranding/CustomBrandingApplicationCustomizer.ts
@@ -18,6 +18,7 @@ export interface ICustomBrandingApplicationCustomizerProperties {
   favicon: string;
   customcss: string;
   fa6css: string;
+  jQuery: string;
 }
 
 /** A Custom Action which can be run during execution of a Client Side Application */
